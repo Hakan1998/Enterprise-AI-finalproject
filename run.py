3b0bb9ed8ed2d5b4beed89c1bf3ab5d3311da496
Hakan1998 <148992293+Hakan1998@users.noreply.github.com>
2024-06-09 15:40:54 +0200

    Create run.py

---
+++ run.py
@@ -0,0 +1,6 @@
+from pipelines import feature_engineering_pipeline, training_pipeline,inference_pipeline
+
+if __name__ == "__main__":
+    feature_engineering_pipeline()
+    training_pipeline()
+    inference_pipeline()
