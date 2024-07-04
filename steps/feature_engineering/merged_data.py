82fad6bc1d25aca5551826d1bcf402bec05f5557
Hakan1998 <148992293+Hakan1998@users.noreply.github.com>
2024-07-04 11:05:33 +0000

    ...

---
+++ steps/feature_engineering/merged_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from typing_extensions import Annotated
+from zenml import step
+
+@step
+def merged_data(clean_data: pd.DataFrame, processed_ratings: pd.DataFrame) -> Annotated[pd.DataFrame, "merged_data"]:
+    """
+    Merges movie and user dataframes on a common key.
+    """
+    # Specify the merge keys and method inside the function
+    merged_data = pd.merge(clean_data, processed_ratings, left_on='id', right_on='movieId', how='inner')
+    # Drop the 'movieId' column from the merged DataFrame as it is redundant with 'id'
+    merged_data.drop(columns='movieId', inplace=True)
+
+    merged_data = merged_data.sample(n=100, random_state=42)  # Ensure reproducibility with random_state
+
+    return merged_data
+
